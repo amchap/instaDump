ca54ad4cfa71499c6fc04d5fc0c2f18cc93074b8
amchap <info@adamchapman.nl>
2022-12-02 12:23:12 +0100

    primary setup instaLoader

---
+++ scripts/instaDownloader.py
@@ -0,0 +1,14 @@
+import instaloader
+
+from instaloader import Profile
+
+L = instaloader.Instaloader()
+
+PROFILE = "demijmering"
+
+profile = Profile.from_username(L.context, PROFILE)
+
+posts_sorted_by_likes = sorted(profile.get_posts(), key = lambda post: post.likes, reverse = True)
+
+for post in posts_sorted_by_likes:
+    L.download_post(post, PROFILE)

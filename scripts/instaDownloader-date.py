5b85fd3785730f098a67dabbe36c1bef6a96200a
amchap <info@adamchapman.nl>
2023-02-23 21:14:48 +0100

    changed naming convention to link with airtable/zapier
    zapier cant import filename, now using the shortcode

--- scripts/instaDownloader-date.py
+++ scripts/instaDownloader-date.py
@@ -3,17 +3,19 @@ from itertools import dropwhile, takewhile
 
 import instaloader
 
-Name = "{filename}"
+PROFILE = "demijmering"
+Name = "{shortcode}"
+
 L = instaloader.Instaloader(filename_pattern=Name)
 
-PROFILE = "demijmering"
-
-posts = instaloader.Profile.from_username(L.context, "demijmering").get_posts()
+posts = instaloader.Profile.from_username(L.context, PROFILE).get_posts()
 profile = instaloader.Profile.from_username(L.context, PROFILE)
+
 
 SINCE = datetime(2023, 12, 1)
 UNTIL = datetime(2023, 1, 1)
 
 for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
-    L.download_post(post, "demijmering")
-    filename = profile.username + '/' + L.format_filename(post, target=profile.username)
+    if not post.is_video:
+        L.download_post(post, PROFILE)
+        filename = profile.username + '/' + L.format_filename(post, target=profile.username)

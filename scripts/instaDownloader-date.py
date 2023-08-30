6e1a963f25ea89c9a4cf7f723a3d03f5c0d05bab
amchap <info@adamchapman.nl>
2023-08-30 22:04:17 +0200

    add photo

--- scripts/instaDownloader-date.py
+++ scripts/instaDownloader-date.py
@@ -13,7 +13,7 @@ profile = instaloader.Profile.from_username(L.context, PROFILE)
 
 
 SINCE = datetime(2023, 12, 1)
-UNTIL = datetime(2023, 8, 1)
+UNTIL = datetime(2023, 8, 15)
 
 for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
     L.download_post(post, PROFILE)
